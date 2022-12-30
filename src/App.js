c0681019418bfcabf30f35867f7ca8f870b4e062
sachdevavaibhav <sachdevavaibhav.2001@gmail.com>
2022-12-30 16:49:38 +0530

    understanding functional components

--- src/App.js
+++ src/App.js
@@ -1,50 +1,75 @@
-import { Component } from 'react'
+// import { Component } from 'react'
+import { useState, useEffect } from 'react'
 import CardList from './components/card-list/card-list.component'
 import SearchBox from './components/search-box/search-box.component'
 
 import './App.css'
 
-class App extends Component {
+const App = () => {
+  const [monsters, setMonsters] = useState([]) // [current state value(value), function to update value(setValue)]
+  const [searchField, setSearchField] = useState('') // [current state value(value), function to update value(setValue)]
 
-  constructor() {
-    super()
-    this.state = { //this is local state of the component and only the component knows about it.
-      monsters: [],
-      searchField: ''
-    }
-  }
+  console.log('render')
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then((response) => response.json())
+  .then((users) => setMonsters(users))
 
-  componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then((response) => response.json())
-      .then((users) => this.setState(
-        () => {
-          return {monsters:users}
-        }
-      ))
-  }
+  const foundMonsters = monsters.filter((monster) => monster.name.toLocaleLowerCase().includes(searchField))
 
-  onSearchChange = (event) => {
-    const searchField = event.target.value.toLocaleLowerCase()
-    this.setState(() => {
-      return {searchField: searchField}
-    })
+  const onSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLocaleLowerCase()
+    setSearchField(searchFieldString)
   }
-
-  render() {
-    // console.log('render from App.js')
-    const {monsters, searchField} = this.state
-    const {onSearchChange} = this
-    const foundMonsters = monsters.filter((monster) => monster.name.toLocaleLowerCase().includes(searchField))
-    
-    return (
-      <div className="App">
-        <h1 className="app-title">Monster Rolodex</h1>
-        <SearchBox onChangeHandler={onSearchChange} placeholder='search monster' className='monster-search-box'/>
-        <CardList monsters={foundMonsters} />
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <h1 className="app-title">Monster Rolodex</h1>
+      <SearchBox onChangeHandler={onSearchChange} placeholder='search monster' className='monster-search-box'/>
+      <CardList monsters={foundMonsters} />
+    </div>
+  )
 }
+
+// class App extends Component {
+
+//   constructor() {
+//     super()
+//     this.state = { //this is local state of the component and only the component knows about it.
+//       monsters: [],
+//       searchField: ''
+//     }
+//   }
+
+//   componentDidMount() {
+//     fetch('https://jsonplaceholder.typicode.com/users')
+//       .then((response) => response.json())
+//       .then((users) => this.setState(
+//         () => {
+//           return {monsters:users}
+//         }
+//       ))
+//   }
+
+//   onSearchChange = (event) => {
+//     const searchField = event.target.value.toLocaleLowerCase()
+//     this.setState(() => {
+//       return {searchField: searchField}
+//     })
+//   }
+
+//   render() {
+//     // console.log('render from App.js')
+//     const {monsters, searchField} = this.state
+//     const {onSearchChange} = this
+//     const foundMonsters = monsters.filter((monster) => monster.name.toLocaleLowerCase().includes(searchField))
+    
+//     return (
+//       <div className="App">
+//         <h1 className="app-title">Monster Rolodex</h1>
+//         <SearchBox onChangeHandler={onSearchChange} placeholder='search monster' className='monster-search-box'/>
+//         <CardList monsters={foundMonsters} />
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,6 +1,18 @@
 import { Component } from "react"
 import './card.styles.css'
 
+// Functional component in React
+// const Card = (props) => {
+//     return (
+//         <div key={props.id} className='card-container'>
+//             <img alt={`monster ${props.name}`} src={`https://robohash.org/${props.id}?set=set2&size=180x180`} />
+//             <h2>{props.name}</h2>
+//             <p>{props.email}</p>
+//         </div>
+//     )
+// }
+
+// Class component in React
 class Card extends Component {
     render()  {
         const {name, email, id} = this.props  
@@ -15,3 +27,15 @@ class Card extends Component {
 }
 
 export default Card
+
+// What is the difference between functional and class component?
+// Functional component is a simple function that returns some JSX
+// Class component is a class that extends Component and has a render method that returns some JSX
+
+// What is the difference between state and props?
+// State is an object that describes your application
+// Props is an object that gets passed to the component
+
+// What is the difference between impuer and pure function?
+// Impure function is a function that changes something outside of its scope
+// Pure function is a function that does not change anything outside of its scope
